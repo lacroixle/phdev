36932c52a7626aa321d9cd38402d9cd49127c33d
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2021-12-19 15:29:13 +0100

    Updated sn_sequence.py

--- tools/sn_sequence.py
+++ tools/sn_sequence.py
@@ -2,6 +2,8 @@
 
 import sys
 import pathlib
+import argparse
+import os
 
 import ztfimg.science
 import ztfimg.stamps
@@ -11,16 +13,41 @@ import astropy.coordinates
 import numpy as np
 from PIL import Image
 
-ztfname = sys.argv[1]
-zfilter = sys.argv[2]
-lc_folder = pathlib.Path(sys.argv[3])
-coords_file = pathlib.Path(sys.argv[4])
+
+zfilters = ['zr', 'zg', 'zi']
+
+
+argparser = argparse.ArgumentParser(description="SN stamp sequence extractor.")
+argparser.add_argument("--output", type=pathlib.Path, help="Output folder.", required=True)
+argparser.add_argument("--ztfname", type=str, help="SN to process.", required=True)
+argparser.add_argument('--zfilter', type=str, choices=zfilters, required=True)
+argparser.add_argument('-j', dest='n_jobs', type=int, default=1)
+argparser.add_argument('--lc-folder', dest='lc_folder', type=pathlib.Path, required=True)
+argparser.add_argument('--cosmodr', type=pathlib.Path)
+
+args = argparser.parse_args()
+
+ztfname = args.ztfname
+zfilter = args.zfilter
+lc_folder = args.lc_folder.expanduser().resolve()
+n_jobs = args.n_jobs
+
+if args.cosmodr:
+    cosmo_dr_folder = args.cosmodr
+elif 'COSMO_DR_FOLDER' in os.environ.keys():
+    cosmo_dr_folder = pathlib.Path(os.environ.get("COSMO_DR_FOLDER"))
+else:
+    print("Cosmo DR folder not set! Either set COSMO_DR_FOLDER environnement variable or use the --cosmodr parameter.")
+    exit(-1)
+
+coords_file = cosmo_dr_folder.joinpath("ztfdr2_coords.csv")
+
 
 lc_df = pd.read_hdf(lc_folder.joinpath("{}.hd5".format(ztfname)), key='lc_{}'.format(zfilter))
 coords_df = pd.read_csv(coords_file, sep=" ", index_col='ztfname')
 
-sn_ra = coords_df.loc[ztfname]['sn_ra']
-sn_dec = coords_df.loc[ztfname]['sn_dec']
+sn_ra = coords_df.loc[ztfname]['host_ra']
+sn_dec = coords_df.loc[ztfname]['host_dec']
 
 def extract_stamp(sciimg_file):
    quadrant = ztfimg.science.ScienceQuadrant.from_filename(sciimg_file)
@@ -28,20 +55,21 @@ def extract_stamp(sciimg_file):
    x, y = quadrant.wcs.world_to_pixel(astropy.coordinates.SkyCoord(sn_ra, sn_dec, unit='deg'))
    print(".", end="", flush=True)
 
-   return ztfimg.stamps.stamp_it(quadrant.get_data('clean'), x, y, 20).data
+   return np.asarray(ztfimg.stamps.stamp_it(quadrant.get_data('clean'), x, y, 40, asarray=True))
 
-stamps = Parallel(n_jobs=8)(delayed(extract_stamp)(sciimg_file) for sciimg_file in lc_df.iloc[:10])
+stamps = Parallel(n_jobs=n_jobs)(delayed(extract_stamp)(sciimg_file) for sciimg_file in lc_df)
 
 print("")
 print("Building stamp tensor...")
-stamps = np.stack([np.array(stamp) for stamp in stamps])
-
-print(stamps)
+stamps = np.nan_to_num(np.stack(stamps))
+print(np.min(stamps), np.max(stamps))
 
 print("Normalizing...")
-stamps = (stamps*np.nanmax(stamps)).astype(np.uint8)
-
-print(stamps)
+#stamps = (stamps*np.nanmax(stamps)).astype(np.uint8)
+stamps = (stamps - np.min(stamps))/(np.max(stamps) - np.min(stamps))
+stamps = (stamps*255.).astype(np.uint8)
+print(np.min(stamps), np.max(stamps))
+#print(stamps)
 
 print("Saving")
 for i, stamp in enumerate(stamps):
