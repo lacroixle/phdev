30fa55200c79e5e286af8645bbc0c6ac1af10722
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2022-01-04 12:26:53 +0100

    Improved download_lc_files.py

--- tools/download_lc_files.py
+++ tools/download_lc_files.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 import sys
 import time
+import argparse
+import pathlib
 
 import pandas as pd
-import pathlib
 from ztfimg import science
 from joblib import Parallel, delayed
 
-ztfname = sys.argv[1]
-lc_folder = pathlib.Path(sys.argv[2])
 
-n_jobs = 1
-if len(sys.argv) > 3:
-    n_jobs = int(sys.argv[3])
+filtercodes = ['zr', 'zg', 'zi']
+
+
+argparser = argparse.ArgumentParser(description="")
+argparser.add_argument('--ztfname', type=str, help="", required=True)
+argparser.add_argument('--lc-folder', dest='lc_folder', type=pathlib.Path, help="", required=True)
+argparser.add_argument('--filtercode', type=str, choices=filtercodes, help="")
+argparser.add_argument('-j', dest='n_jobs', type=int, default=1, help="")
+args = argparser.parse_args()
+
+ztfname = args.ztfname
+lc_folder = args.lc_folder
+n_jobs = args.n_jobs
+if args.filtercode:
+    print("hey")
+    filtercodes = [args.filtercode]
 
 # Image size in MB
 sciimg_size = 38
@@ -50,15 +62,14 @@ def download_lc(hdfstore, filter_key):
     print("")
 
 
-zfilters = ['zr', 'zg', 'zi']
 start_dl_time = time.perf_counter()
 
-for zfilter in zfilters:
+for filtercode in filtercodes:
     with pd.HDFStore(lc_folder.joinpath("{}.hd5".format(ztfname)), mode='r') as hdfstore:
-        if '/lc_{}'.format(zfilter) in hdfstore.keys():
-            download_lc(hdfstore, 'lc_{}'.format(zfilter))
+        if '/lc_{}'.format(filtercode) in hdfstore.keys():
+            download_lc(hdfstore, 'lc_{}'.format(filtercode))
         else:
-            print("No {} filter found for {}!".format(zfilter, ztfname))
+            print("No {} filter found for {}!".format(filtercode, ztfname))
 
 total_elapsed_time = time.perf_counter() - start_dl_time
 print("Total time elapsed={}".format(total_elapsed_time))
