706a649ad31061209a7d4fd041d2b4fd57bc5f02
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2022-01-05 15:09:38 +0100

    Added poloka preparation script

--- tools/prepare_poloka.py
+++ tools/prepare_poloka.py
@@ -15,7 +15,7 @@ poloka_dir = pathlib.Path(sys.argv[3])
 sn_df = pd.read_csv(sn_list_file, sep=",", comment="#")
 sn_df.set_index("ztfname", inplace=True)
 
-dry_run = True
+copy_files = True
 
 zfilters = ['zg', 'zr', 'zi']
 
@@ -25,7 +25,7 @@ for sn in sn_df.index:
     def _create_subfolders(zfilter):
         lc_df = pd.read_hdf(lc_dir.joinpath("{}.hd5".format(sn)), key='lc_{}'.format(zfilter))
 
-        for sciimg_filename_fits in lc_df:
+        for sciimg_filename_fits in lc_df['ipac_file']:
             # First create filter path
 
             poloka_dir.joinpath("{}/{}".format(sn, zfilter)).mkdir(exist_ok=True)
@@ -38,9 +38,10 @@ for sn in sn_df.index:
             sciimg_path = pathlib.Path(ztfquery.io.get_file(sciimg_filename_fits, downloadit=False, suffix='sciimg.fits'))
             mskimg_path = patylib.Path(ztfquery.io.get_file(sciimg_filename_fits, downloadit=False, suffix='mskimg.fits'))
 
-            if not dry_run:
+            if copy_files:
                 shutil.copy2(sciimg_path, folder_path)
                 shutil.copy2(mskimg_path, folder_path)
+
 
     with pd.HDFStore(lc_dir.joinpath("{}.hd5".format(sn)), mode='r') as hdfstore:
         for zfilter in zfilters:
