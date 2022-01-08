9e748e684bbb940a9f85f82ea7884f7c7e9e0436
Llacroix Llacroix <llacroix@cca007.in2p3.fr>
2022-01-08 16:22:16 +0100

    prepare_poloka.py: vastly improved, now realy usable (not yet fully tested though)

--- tools/deploy_poloka.py
+++ tools/deploy_poloka.py
@@ -5,54 +5,62 @@ import pathlib
 import subprocess
 import logging
 import datetime
+import os
 
 from joblib import Parallel, delayed
 
 
 filtercodes = ['zg', 'zr', 'zi']
+poloka_fct = []
 
-argparser = argparse.ArgumentParser(description="")
-argparser.add_argument('--ztfname', type=str, required=True)
-argparser.add_argument('-j', dest=n_jobs, type=int, default=1)
-argparser.add_argument('--wd', type=pathlib.Path, help="Working directory")
-argparser.add_argument('--filtercode', choices=filtercodes)
-argparser.add_argument('--func')
-
-args.argparser.parse_args()
 
 def run_and_log(cmd, logger=None):
     out = subprocess.run(cmd, capture_output=True)
     if logger:
-        logger.info(out.stdout)
-        logger.error(out.stderr)
+        logger.info(out.stdout.decode('utf-8'))
+        logger.error(out.stderr.decode('utf-8'))
 
     return out.returncode
 
+poloka_fct.append(run_and_log)
+
 
 def make_catalog(folder, logger):
-
-    run_and_log(folder, logger)
+    run_and_log(["make_catalog", folder], logger)
     return folder.joinpath("se.list").exists()
+
+poloka_fct.append(make_catalog)
 
 
 def mkcat2(folder, logger):
-    pass
+    run_and_log(["mkcat2", folder], logger)
+    return folder.joinpath("standalone_stars.list").exists()
+
+poloka_fct.append(mkcat2)
 
 
 def makepsf(folder, logger):
-    pass
+    run_and_log(["makepsf", folder], logger)
+    return folder.joinpath("psfstars.list").exists()
+
+poloka_fct.append(makepsf)
 
 
-def all_cmd(folder):
+def pipeline(folder, logger):
+    if not make_catalog(folder, logger):
+        return False
 
-    if not make_catalog(folder):
-        pass
+    if not mkcat2(folder, logger):
+        return False
 
-    if not mkcat2(folder):
-        pass
+    if not makepsf(folder, logger):
+        return False
 
-    if not makepsf(folder):
-        pass
+    return True
+
+poloka_fct.append(pipeline)
+
+poloka_fct = dict(zip([fct.__name__ for fct in poloka_fct], poloka_fct))
 
 
 def launch(folder, fct):
@@ -61,13 +69,45 @@ def launch(folder, fct):
     logger.setLevel(logging.INFO)
     logger.info(datetime.datetime.today())
 
-    fct(folder, logger)
+    result = fct(folder, logger)
 
-    print(".", end="", flush=True)
+    if result:
+        print(".", end="", flush=True)
+    else:
+        print("x", end="", flush=True)
+
     logger.info("Done.")
+    return result
 
-folders = args.wd.glob()
 
-results = Parallel(n_jobs=n_jobs)(delayed(launch)(folder, make_catalog) for folder in foldersfct)
+argparser = argparse.ArgumentParser(description="")
+argparser.add_argument('--ztfname', type=str, required=True)
+argparser.add_argument('-j', dest='n_jobs', type=int, default=1)
+argparser.add_argument('--wd', type=pathlib.Path, help="Working directory")
+argparser.add_argument('--filtercode', choices=filtercodes)
+argparser.add_argument('--func', choices=poloka_fct.keys(), default='pipeline')
 
-[print(folder) for folder, result in zip(folders, results) if result]
+args = argparser.parse_args()
+
+
+print("Running Poloka function {} on {}-{}".format(args.func, args.ztfname, args.filtercode))
+
+# First check if there is quadrants associated with the selected filter
+if not args.wd.joinpath("{}/{}".format(args.ztfname, args.filtercode)).exists():
+    print("No quadrant for filter {}! Skipping.".format(args.filtercode))
+
+cwd = args.wd.joinpath("{}/{}".format(args.ztfname, args.filtercode))
+os.chdir(cwd)
+
+folders = list(cwd.glob("*"))
+
+print("Found {} quadrant folder.".format(len(folders)))
+results = Parallel(n_jobs=args.n_jobs)(delayed(launch)(folder, wholeseq) for folder in folders)
+
+print("")
+
+# Print quadrant that failed
+if any not results:
+    [print(folder) for folder, result in zip(folders, results) if not result]
+
+    print("")
