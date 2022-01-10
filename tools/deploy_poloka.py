4745d387a1e573ae866b6bad05496ba5b2a06011
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2022-01-10 19:45:50 +0100

    deploy_poloka.py: proper map-reduce computing scheme. Added statistics capabilities.

--- tools/deploy_poloka.py
+++ tools/deploy_poloka.py
@@ -6,8 +6,14 @@ import subprocess
 import logging
 import datetime
 import os
+import time
 
 from joblib import Parallel, delayed
+import pandas as pd
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+import list_format
 
 
 filtercodes = ['zg', 'zr', 'zi']
@@ -22,28 +28,28 @@ def run_and_log(cmd, logger=None):
 
     return out.returncode
 
-poloka_fct.append(run_and_log)
+poloka_fct.append({'map': run_and_log})
 
 
 def make_catalog(folder, logger):
     run_and_log(["make_catalog", folder], logger)
     return folder.joinpath("se.list").exists()
 
-poloka_fct.append(make_catalog)
+poloka_fct.append({'map': make_catalog})
 
 
 def mkcat2(folder, logger):
     run_and_log(["mkcat2", folder], logger)
     return folder.joinpath("standalone_stars.list").exists()
 
-poloka_fct.append(mkcat2)
+poloka_fct.append({'map': mkcat2})
 
 
 def makepsf(folder, logger):
     run_and_log(["makepsf", folder], logger)
     return folder.joinpath("psfstars.list").exists()
 
-poloka_fct.append(makepsf)
+poloka_fct.append({'map': makepsf})
 
 
 def pipeline(folder, logger):
@@ -58,7 +64,7 @@ def pipeline(folder, logger):
 
     return True
 
-poloka_fct.append(pipeline)
+poloka_fct.append({'map': pipeline})
 
 
 files_to_keep = ["elixir.fits", "mask.fits", "calibrated.fits", ".dbstuff"]
@@ -78,9 +84,86 @@ def clean(folder, logger):
 
     return True
 
-poloka_fct.append(clean)
+poloka_fct.append({'map': clean})
 
-poloka_fct = dict(zip([fct.__name__ for fct in poloka_fct], poloka_fct))
+
+# Extract data from standalone stars and plot several distributions
+def stats(folder, logger):
+    def _extract_from_list(list_filename, hdfstore):
+        list_path = folder.joinpath(list_filename).with_suffix(".list")
+
+        if not list_path.exists():
+            return False
+
+        with open(list_path, mode='r') as f:
+            global_params, df = list_format.read_list(f)
+
+        hdfstore.put(list_path.stem, df)
+        hdfstore.put("{}_globals".format(list_path.stem), pd.DataFrame([global_params]))
+
+        return True
+
+
+    import warnings
+    warnings.filterwarnings('ignore',category=pd.io.pytables.PerformanceWarning)
+
+    with pd.HDFStore(folder.joinpath("lists.hdf5"), mode='w') as hdfstore:
+
+        # From make_catalog
+        _extract_from_list("se", hdfstore)
+
+        # From mkcat2
+        cont = _extract_from_list("standalone_stars", hdfstore)
+
+        if not cont:
+            return True
+
+        _extract_from_list("aperse", hdfstore)
+
+        # From calibrated.fits
+        keywords = ['sexsky', 'sexsigma', 'bscale', 'bzero', 'origsatu', 'saturlev', 'backlev', 'back_sub', 'seseeing', 'gfseeing']
+
+        calibrated = {}
+        with fits.open(folder.joinpath("calibrated.fits")) as hdul:
+            for keyword in keywords:
+                calibrated[keyword] = hdul[0].header[keyword]
+
+            hdfstore.put('calibrated', pd.DataFrame([calibrated]))
+
+        # From makepsf
+        cont = _extract_from_list("psfstars", hdfstore)
+
+        if not cont:
+            return True
+
+        _extract_from_list("psftuples", hdfstore)
+
+    return True
+
+
+def stats_reduce(cwd, ztfname, filtercode):
+    # Seeing histogram
+
+    folders = [folder for folder in cwd.glob("*") if folder.is_dir()]
+
+    seseeings = []
+    for folder in folders:
+        hdfstore_path = folder.joinpath("lists.hdf5")
+        if hdfstore_path.exists():
+            with pd.HDFStore(hdfstore_path, mode='r') as hdfstore:
+                if '/calibrated' in hdfstore.keys():
+                    calibrated_df = hdfstore.get('/calibrated')
+                    seseeings.append(float(calibrated_df['seseeing']))
+
+    plt.hist(seseeings, bins=60, range=[0.5, 3], color='xkcd:dark grey', histtype='step')
+    plt.grid()
+    plt.savefig(cwd.joinpath("{}-{}_seseeing_dist.png".format(ztfname, filtercode)), dpi=300)
+    plt.close()
+
+poloka_fct.append({'map': stats, 'reduce': stats_reduce})
+
+
+poloka_fct = dict(zip([fct['map'].__name__ for fct in poloka_fct], poloka_fct))
 
 
 def launch(folder, fct):
@@ -89,7 +172,12 @@ def launch(folder, fct):
     logger.setLevel(logging.INFO)
     logger.info(datetime.datetime.today())
 
-    result = fct(folder, logger)
+    try:
+        result = fct['map'](folder, logger)
+    except Exception as e:
+        print("")
+        print("In folder {}".format(folder))
+        print(e.with_traceback)
 
     if not args.dry_run:
         if result:
@@ -108,6 +196,8 @@ argparser.add_argument('--wd', type=pathlib.Path, help="Working directory")
 argparser.add_argument('--filtercode', choices=filtercodes)
 argparser.add_argument('--func', type=str, choices=poloka_fct.keys(), default='pipeline')
 argparser.add_argument('--dry-run', dest='dry_run', action='store_true')
+argparser.add_argument('--no-map', dest='no_map', action='store_true')
+argparser.add_argument('--no-reduce', dest='no_reduce', action='store_true')
 
 args = argparser.parse_args()
 args.wd = args.wd.expanduser().resolve()
@@ -122,15 +212,25 @@ if not args.wd.joinpath("{}/{}".format(args.ztfname, args.filtercode)).is_dir():
 cwd = args.wd.joinpath("{}/{}".format(args.ztfname, args.filtercode))
 os.chdir(cwd)
 
-folders = list(cwd.glob("*"))
+folders = [folder for folder in list(cwd.glob("*")) if folder.is_dir()]
 
 print("Found {} quadrant folders".format(len(folders)))
-results = Parallel(n_jobs=args.n_jobs)(delayed(launch)(folder, poloka_fct[args.func]) for folder in folders)
+if not args.no_map:
+
+    map_start = time.perf_counter()
+    results = Parallel(n_jobs=args.n_jobs)(delayed(launch)(folder, poloka_fct[args.func]) for folder in folders)
+    print("")
+    print("Time elapsed={}".format(time.perf_counter() - map_start))
+
+    # Print quadrant that failed
+    if not all(results):
+        [print(folder) for folder, result in zip(folders, results) if not result]
+        print("")
+
+if not args.no_reduce and 'reduce' in poloka_fct[args.func].keys():
+    print("Reducing")
+    reduce_start = time.perf_counter()
+    poloka_fct[args.func]['reduce'](cwd, args.ztfname, args.filtercode)
+    print("Time elapsed={}".format(time.perf_counter() - reduce_start))
 
 print("")
-
-# Print quadrant that failed
-if not all(results):
-    [print(folder) for folder, result in zip(folders, results) if not result]
-
-    print("")
---
+++ tools/list_format.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+
+def read_list(f):
+    # Extract global @ parameters
+    global_params = {}
+    line = f.readline()
+    curline = 1
+    while line[0] == "@":
+        line = line[:-1]
+        splitted = line.split(" ")
+        key = splitted[0][1:]
+
+        value = list(map(float, splitted[1:]))
+        if len(value) == 1:
+            value = value[0]
+        else:
+            value = pd.array(value)
+
+        global_params[key.lower()] = value
+
+        line = f.readline()
+        curline += 1
+
+    # Extract dataframe
+    # First, column names
+    columns = []
+    while line[0] == "#":
+        curline += 1
+        line = line[1:-1].strip()
+
+        splitted = line.split(" ")
+        if splitted[0].strip() == "end":
+            line = f.readline()
+            break
+
+        if splitted[0].strip() == "format":
+            line = f.readline()
+            continue
+
+        splitted = line.split(":")
+        columns.append(splitted[0].strip())
+
+        line = f.readline()
+
+    df = pd.read_csv(f, sep=" ", names=columns, index_col=False)
+
+    return global_params, df
