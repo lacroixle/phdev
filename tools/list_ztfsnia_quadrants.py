d13d6cb8e0ac18b5398c6e86e53b7fc6520a7999
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2022-07-20 15:35:32 +0200

    Added list_ztfsnia_quadrants.py to list all quadrants to dl

---
+++ tools/list_ztfsnia_quadrants.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pathlib
+import argparse
+
+import pandas as pd
+
+import utils
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description="")
+    argparser.add_argument('--lc-folder', type=pathlib.Path)
+
+    args = argparser.parse_args()
+    args.lc_folder = args.lc_folder.expanduser().resolve()
+
+    lc_files = args.lc_folder.glob("*.hd5")
+
+    quadrant_list = []
+    for lc_file in lc_files:
+        with pd.HDFStore(lc_file, mode='r') as hdfstore:
+            for filtercode in utils.filtercodes:
+                if '/lc_{}'.format(filtercode) in hdfstore.keys():
+                    lc_df = pd.read_hdf(hdfstore, key='lc_{}'.format(filtercode))
+                    quadrant_list.extend(lc_df['ipac_file'])
+
+    for quadrant in quadrant_list:
+        print(quadrant)
