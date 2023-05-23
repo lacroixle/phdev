461d7a69a2f106843ecbc8c5d3fe96f970c522e1
Leander (lpnlp202111) <leander.lacroix.dev@protonmail.com>
2023-05-23 15:08:26 +0200

    deppol: added some misc catalog concatenation function.

--- deppol/deppol_misc.py
+++ deppol/deppol_misc.py
@@ -891,3 +891,11 @@ def catalogs_to_ds9regions(exposure, logger, args):
 
 def dummy(lightcurve, logger, args):
     return True
+
+
+def concat_catalogs(lightcurve, logger, args):
+    catalog = lightcurve.extract_star_catalog(['aperstars'])[['exposure', 'x', 'y', 'gmxx', 'gmyy', 'gmxy', 'apfl5', 'rad5']].rename(columns={'apfl5': 'aperflux', 'rad5': 'aperrad'})
+    catalog['ccdid'] = catalog.apply(lambda x: int(x['exposure'][30:32]), axis=1)
+    catalog['qid'] = catalog.apply(lambda x: int(x['exposure'][36]), axis=1)
+    catalog.to_parquet("bigcat.parquet")
+    return True
