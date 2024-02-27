12d1e6a337e87ae88a46fa327e47dcf535b8d50c
luckyjim <jcolley@lpnhe.in2p3.fr>
2024-02-27 17:04:08 +0100

    around this ...

--- deppol/deppol_pocket_effect.py
+++ deppol/deppol_pocket_effect.py
@@ -4,4 +4,14 @@ Created on Feb 27, 2024
 '''
 
 def pocket_effect_cor(exposure, logger, args):
-    pass
+   
+    from ztfsensors import pocket, PocketModel
+    
+    idx_ccd ,idx_quad = 1,1
+    alpha,cmax,beta,nmax =get_coef_model(idx_ccd ,idx_quad)
+  
+    pocket_model = PocketModel(alpha,cmax,beta,nmax)
+    
+    image_with_pe = get_image(idx_ccd ,idx_quad)
+    
+    cs, delta, mask = pocket.correct_2d(pocket_model, image_with_pe)
